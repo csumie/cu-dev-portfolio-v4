4717ded09af3f93b40c66b836ce7f12bc446bf03
csumie <csurdanivia@gmail.com>
2024-05-27 21:18:02 -0400

    updating javascript

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,9 +1,6 @@
 function isScrolledIntoView(elem) {
-  // Declare variable for top of viewport
   var TopViewPort = $(window).scrollTop();
-  // Declare variable for bottom of viewport
   var BotViewPort = TopViewPort + $(window).height();
-  // Declare Bot = Top + viewport
   var TopOfElement = $(elem).offset().top;
   var BotOfElement = TopOfElement + $(elem).height();
   return ((BotOfElement <= BotViewPort) && (TopOfElement >= TopViewPort));
@@ -11,7 +8,6 @@ function isScrolledIntoView(elem) {
 }
 
 $(window).scroll(function () {
-//   Ask if the div is in viewport
 
   $('.animation-about-me').each(function(key, value) {
     var section = 'div.' + $(value).attr('class');
